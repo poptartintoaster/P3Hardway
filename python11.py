6a3d4a5b38cb481d369699d20008640322730189
tility <geogrundy@gmail.com>
2020-08-23 14:36:18 -0500

    Created exercise 11 with alittle bit of Nick Cage for spice.

---
+++ python11.py
@@ -0,0 +1,8 @@
+print("How old is Nick Cage?", end=' ')
+age = input()
+print("How tall is Nick Cage?", end=' ')
+height = input()
+print("How Much does Nick Cage weight?", end=' ')
+weight = input()
+
+print(f"So, Nick Cage is {age} old, {height} tall and {weight} heavy.")
