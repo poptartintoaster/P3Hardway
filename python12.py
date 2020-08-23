f86b56c922579c23d0530876ca83bcd0467e49fb
tility <geogrundy@gmail.com>
2020-08-23 14:41:21 -0500

    Find a different way to to exercise the same goal from exercise 11

---
+++ python12.py
@@ -0,0 +1,5 @@
+age = input("How old is Nick Cage? ")
+height = input("How tall is Nick Cage? ")
+weight = input("How much does Nick Cage weight? ")
+
+print(f"So Nick Cage is {age} years old and {height} tall and {weight} heavy.")
